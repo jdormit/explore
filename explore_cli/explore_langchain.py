1716ed231ba9f13323ca872e24ef8969a5dad756
Jeremy Dormitzer <jeremydormitzer@hummingbird.co>
2024-10-31 00:55:47 -0400

    [WIP] Find documents using glob

--- explore_cli/explore_langchain.py
+++ explore_cli/explore_langchain.py
@@ -1,37 +1,58 @@
 import argparse
+from fnmatch import fnmatch
 import os
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_huggingface import HuggingFaceEmbeddings
+from pathspec import PathSpec
+from pathspec.patterns.gitwildmatch import GitWildMatchPattern
+
+IGNORED_PATTERNS = [
+    "**/.git/*",
+    "*.tmp",
+    "*.log",
+    "*.swp",
+    "*.bak",
+    "**/node_modules/*",
+    "*.sock",
+]
 
 
 def collection_name(directory):
     return directory.replace("/", "_").strip("_")
 
 
-def collect_documents(directory):
-    loader = DirectoryLoader(
-        directory,
-        glob="**/*",
-        exclude=[
-            "**/.git/*",
-            "*.tmp",
-            "*.log",
-            "*.swp",
-            "*.bak",
-            "**/node_modules/*",
-            "**/.venv/*",
-            "*.sock",
-        ],
-        loader_cls=TextLoader,
-        loader_kwargs={"autodetect_encoding": True},
-        silent_errors=True,
-        use_multithreading=True,
-        show_progress=True,
-    )
-    # TODO: skip documents that haven't changed
+def load_gitignore(directory):
+    gitignore_path = os.path.join(directory, ".gitignore")
+    if os.path.exists(gitignore_path):
+        with open(gitignore_path, "r") as f:
+            return PathSpec.from_lines(GitWildMatchPattern, f)
+    return None
+
+
+def collect_documents(directory, use_gitignore=True):
+    pathspec = load_gitignore(directory) if use_gitignore else None
+    docs = []
+    # TODO: skip files that haven't changed
+    for root, _, dir_files in os.walk(directory):
+        for file in dir_files:
+            if not (
+                any(
+                    fnmatch(os.path.join(root, file), pattern)
+                    for pattern in IGNORED_PATTERNS
+                )
+                or (pathspec and pathspec.match_file(file))
+            ):
+                try:
+                    docs.extend(
+                        TextLoader(
+                            file_path=os.path.join(root, file), autodetect_encoding=True
+                        ).load()
+                    )
+                except Exception as e:
+                    print(f"Error loading {os.path.join(root, file)}: {e}")
     # TODO: split documents into chunks
-    return loader.load()
+    return docs
 
 
 def main():
